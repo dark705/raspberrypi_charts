636619cc53ec75bd8cd7c6f80e522e8fc84b20fc
dark705 <dark705r@mail.ru>
2018-03-13 02:25:47 +0300

    Add time show on summary items

--- views/summary.php
+++ views/summary.php
@@ -1,31 +1,37 @@
 <?php
 $urlCommon = 'http://' . $_SERVER['SERVER_ADDR'] . pathinfo($_SERVER['PHP_SELF'])["dirname"] . '/json/';
 
-$urlElectro = $urlCommon . 'json-pzem004t.php?last';
-$urlWeather = $urlCommon . 'json-dht22.php?last';
-$electro = json_decode(file_get_contents($urlElectro))[0]; //get only one, last value
-$weather = json_decode(file_get_contents($urlWeather))[0];
+$urlPzem004t = $urlCommon . 'json-pzem004t.php?last';
+$urlDht22 = $urlCommon . 'json-dht22.php?last';
+$pzem004t = json_decode(file_get_contents($urlPzem004t))[0]; //get only one, last value
+$dht22 = json_decode(file_get_contents($urlDht22))[0];
 
-//Get sensors names
+//Get ds18b20 names
 $urlSensorsNames = $urlCommon. 'json-ds18b20.php?names';
 $sensorsNames = json_decode(file_get_contents($urlSensorsNames));
 
-//For each sensor
+//For each ds18b20
 foreach ($sensorsNames as  $serial => $name){
 	$urlSensor = $urlCommon . "json-ds18b20.php?serial=$serial".'&last';
-	$temperatureSensor = json_decode(file_get_contents($urlSensor))[0][1];//get only one, last value, and only temp
-	$sensors[$serial] = array('name' => $name, 'temperature' => $temperatureSensor);//Associate array, key as serial
+	$sensor = json_decode(file_get_contents($urlSensor))[0];//get only one, last value
+	$temperature = $sensor[1];
+	$time = $sensor[0];
+	$sensors[$serial] = array('time'=> $time, 'name' => $name, 'temperature' => $temperature);//Associate array, key as serial
 }
 
-$voltage = $electro[1];
-$current = $electro[2];
-$power = $electro[3];
-$temperature = $weather[1];
-$humidity = $weather[2];
+$time_electro =  $pzem004t[0];
+$voltage = $pzem004t[1];
+$current = $pzem004t[2];
+$power = $pzem004t[3];
+
+$time_wheather =  $dht22[0];
+$temperature = $dht22[1];
+$humidity = $dht22[2];
 ?>
 
 <div class="item">
 <h3>Электросеть:</h3>
+<p class="ontime">(показания на <?=gmdate("Y-m-d H:i:s", $time_electro)?>)</p>
 <p>Напряжение: <?=$voltage?></p>
 <p>Ток: <?=$current?></p>
 <p>Мощность: <?=$power?></p>
@@ -33,6 +39,7 @@ $humidity = $weather[2];
 
 <div class="item">
 <h3>Погода:</h3>
+<p class="ontime">(показания на <?=gmdate("Y-m-d H:i:s", $time_wheather)?>)</p>
 <p>Температура: <?=$temperature?></p>
 <p>Влажность: <?=$humidity?></p>
 </div>
@@ -40,6 +47,7 @@ $humidity = $weather[2];
 <?foreach($sensors as $sensor):?>
 	<div class="item">
 	<h3><?=$sensor['name']?></h3>
+	<p class="ontime">(показания на <?=gmdate("Y-m-d H:i:s", $sensor['time'])?>)</p>
 	<p>Температура: <?=$sensor['temperature']?></p>
 	</div>
 <?endforeach;?>
