bad5bb37ad4eb659ffca60963ccb0e58d3386fc7
dark705 <dark705r@mail.ru>
2019-12-17 01:24:57 +0300

    Use PSR-4 composer class autoloader

--- service/temperature.php
+++ service/temperature.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../php/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use PDO\MySQL;
 use Device\DS18B20;
--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../php/autoload.php');
+require_once('../vendor/autoload.php');
 
 use DAO\SensorsFactory;
 use Models\Template;
